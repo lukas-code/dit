9a5ef01a8f0689bcc2904f3afa80041ab4f62b2e
Lukas Markeffsky <@>
2023-07-05 08:04:23 +0000

    fix broken docs

--- dit/src/lib.rs
+++ dit/src/lib.rs
@@ -1,5 +1,6 @@
 #![deny(rust_2018_idioms)]
 #![warn(missing_debug_implementations)]
+#![deny(rustdoc::broken_intra_doc_links)]
 
 use clap::{Parser, Subcommand};
 use dit_core::peer::{Config, DhtAddr, DhtAndSocketAddr, Runtime};
